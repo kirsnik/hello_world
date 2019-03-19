19305f22c60b77c7e89865fdbdcf8cdfdb13d1d7
Rene.Mendes <gwingster@gmail.com>
2019-03-19 11:53:24 -0700

    rework

--- js/pure.js
+++ js/pure.js
@@ -6,3 +6,13 @@ function myFunction() {
     x.style.display = "block";
   }
 }
+
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
